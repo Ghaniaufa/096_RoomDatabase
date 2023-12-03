21b4364e3d5d97428b607023d2b264f2abee078e
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-03 23:38:33 +0700

    Create new method ad PenyediaViewModel for use Object PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,15 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
