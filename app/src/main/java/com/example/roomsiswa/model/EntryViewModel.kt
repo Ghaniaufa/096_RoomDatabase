f0cc0fe2c9add0055d846607cf802e95d798c366
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-03 21:52:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.roomsiswa.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
+import com.example.roomsiswa.repositori.OfflineRepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa) :ViewModel() {
+
+    /**
+     * Berisi Status Siswa Saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+    private set
+
+    /** Fungsi untuk memvalidasi Input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /**Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa() {
+        if (validasiInput()) {
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+
+}
+/** 4. Bikin class UIStateSiswa*/
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+/** 1. Bikin Detail Siswa
+Fungsinya untuk mengkonversi datainput ke data dalam tabel seusai jenis datanya*/
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String =""
+)
+
+/** 5. Bikin Fun DetailSiswa > ToSiswa */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+/** 3. Bikin Fun Siswa (toUiStateSiswa)*/
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+/** 2. Bikin Function Siswa*/
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
