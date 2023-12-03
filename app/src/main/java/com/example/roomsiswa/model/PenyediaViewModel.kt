296f3ff0e6a0f775406e70eff4674bf9582b36bd
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-03 23:41:00 +0700

    Create new Function for Extras and use for HomeViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.ui.theme.AplikasiSiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -13,3 +16,11 @@ object PenyediaViewModel {
         }
     }
 }
+
+/**
+ * fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
+ * [AplikasiSiswa].
+ */
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
