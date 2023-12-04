4e854af14e457700debe0441d690fa639302e178
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-04 21:35:59 +0700

    Insert new at AndroidManifest.xml "AplikasiSiswa"

--- app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
