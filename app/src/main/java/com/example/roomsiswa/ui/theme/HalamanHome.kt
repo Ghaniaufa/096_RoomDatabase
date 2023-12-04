ba5624cc222fef2112110d39dd52666e05a8af95
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-12-04 21:45:10 +0700

    Create new Method LazyColumn for the List Data Siswa

--- app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/HalamanHome.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Phone
@@ -111,7 +113,18 @@ fun BodyHome(
 fun ListSiswa(
     itemSiswa: List<Siswa>,
     modifier : Modifier=Modifier
-){}
+){
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+            person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
 
 @Composable
 fun DataSiswa(
